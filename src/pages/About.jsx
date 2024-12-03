99734fc5d8afdccbbe8393765136fe9b69e70572
creddy9 <creddy9@ford.com>
2024-12-03 10:18:55 +0530

    changes commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { asset } from '../assets/frontend_assets/assets'
+import NewsLetter from '../components/NewsLetter'
 
 const About = () => {
   return (
@@ -21,8 +22,20 @@ const About = () => {
             <Title text1={'WHY'} text2={'CHOOSE US'} />
       </div>
       <div className='flex flex-col md:flex-row text-sm mb-20'>
-
+            <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+                  <b>Quality Assurance:</b>
+                  <p className='text-gray-600'>We meticulously and vet each product to ensure it meets our stringent</p>
+            </div>
+            <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+                  <b>Convenience:</b>
+                  <p className='text-gray-600'>We meticulously and vet each product to ensure it meets our stringent</p>
+            </div>
+            <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+                  <b>Exceptional Customer Service:</b>
+                  <p className='text-gray-600'>We meticulously and vet each product to ensure it meets our stringent</p>
+            </div>
       </div>
+      <NewsLetter />
     </div>
   )
 }
